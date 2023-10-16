94c58c4a2b67cef2875cca1f13fa1e934f08f61e
jack mcqueen <jdmcqueen44@gmail.com>
2023-10-16 16:30:53 -0400

    STYLING: Add Tailwind package & colour profiles

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      black: "#000000",
+      white: "#FFFFFF",
+      tangerine: "#E99D86",
+      rufous: "#A42208",
+      smoke: "#F6F6F3",
+      alabaster: "#E4E4DD",
+      onyx: "#313638",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
