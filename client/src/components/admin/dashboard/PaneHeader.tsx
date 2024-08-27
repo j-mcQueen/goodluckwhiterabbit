03e438bfa18d55617e7dcea09ba11cb53f3f16f0
jack mcqueen <jdmcqueen44@gmail.com>
2024-08-27 18:56:34 -0400

    Match styling of pane header to new design

--- client/src/components/admin/dashboard/PaneHeader.tsx
+++ client/src/components/admin/dashboard/PaneHeader.tsx
@@ -5,18 +5,18 @@ export default function PaneHeader({ ...props }) {
   const { paneTitle, setActivePane } = props;
 
   return (
-    <header className="flex justify-between items-center">
-      <h2 className="italic tracking-wider">{paneTitle}</h2>
+    <header className="flex justify-between items-center pb-5">
+      <h2 className="text-xl">{paneTitle}</h2>
 
       <button
         type="button"
-        className="border border-solid border-mag drop-shadow-mag xl:hover:border-red xl:hover:drop-shadow-red w-10 h-10 flex items-center justify-center xl:focus:border-red xl:focus:drop-shadow-red outline-none transition-colors"
+        className="border border-solid border-white xl:hover:border-rd w-10 h-10 flex items-center justify-center focus:border-red outline-none transition-colors"
         onClick={() => {
           setActivePane("ALL");
           new Audio(clicky).play();
         }}
       >
-        <Close className={"w-[18px] h-[18px]"} customColor={"#FFF"} />
+        <Close className="w-4 4" customColor={"#FFF"} />
       </button>
     </header>
   );
