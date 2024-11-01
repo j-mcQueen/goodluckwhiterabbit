ee4b2ded9aa3b408f1b3f29cb40dfea65a03a9a8
jack mcqueen <jdmcqueen44@gmail.com>
2024-11-01 17:29:58 -0400

    Improve performance of zip creation and download

--- client/src/components/user/dashboard/Views.tsx
+++ client/src/components/user/dashboard/Views.tsx
@@ -1,10 +1,6 @@
 import { useEffect, useState } from "react";
-import { handleDownload } from "./utils/handleDownload";
 import { createZip } from "./utils/createZip";
 import { executeGenerationChain } from "../../global/utils/executeGenerationChain";
-import { AnimatePresence, motion } from "framer-motion";
-
-import StarFilled from "../../../assets/media/icons/StarFilled";
 import Carousel from "./views/Carousel";
 import Grid from "./views/Grid";
 import Loading from "../../global/Loading";
@@ -50,32 +46,14 @@ export default function Views({ ...props }) {
   return (
     <>
       <div className="flex flex-col">
-        <div className="flex items-center justify-between pr-3">
-          <h1 className="text-white xl:text-2xl py-5 max-w-[240px] pl-3">
-            {user.name.toUpperCase()}
-          </h1>
-
-          <div className="text-white flex items-center gap-5 pl-3">
-            {spinner ? <Loading /> : null}
-
-            <button
-              type="button"
-              className="border border-solid border-white text-lg flex gap-1 items-center py-1 px-3 xl:hover:border-rd xl:focus:border-rd transition-colors"
-              onClick={async () => {
-                const url = await createZip(favourites);
-                return handleDownload(
-                  `data:application/zip;base64,${url}`,
-                  `${user.name}-${activeImageset}.zip`
-                );
-              }}
-            >
-              DOWNLOAD: <StarFilled className="w-5 h-5" red={true} />
-            </button>
-          </div>
-        </div>
+        <h1 className="text-white xl:text-2xl py-5 max-w-[240px] pl-3">
+          {user.name.toUpperCase()}
+        </h1>
       </div>
 
       <div className="absolute bottom-[25px] left-[25px] z-10 text-lg">
+        {spinner ? <Loading /> : null}
+
         <p className="text-white text-center pb-2">
           LOADED:{" "}
           {
@@ -85,48 +63,33 @@ export default function Views({ ...props }) {
           / {user.fileCounts[activeImageset]}
         </p>
 
-        <AnimatePresence>
-          {imageset.filter((item: object) => item instanceof File === true)
-            .length < user.fileCounts[activeImageset] && (
-            <motion.button
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              type="button"
-              disabled={disabled}
-              className="text-white border border-solid border-rd py-1 px-3 bg-black"
-              onClick={handleClick}
-            >
-              LOAD MORE
-            </motion.button>
-          )}
-        </AnimatePresence>
-
-        <AnimatePresence>
-          {user.fileCounts[activeImageset] ===
-            imageset.filter((item: object) => item instanceof File === true)
-              .length && (
-            // encourages user to look through all their files first before downloading
-            // hugely resource intensive operation if immediately available
-            <motion.button
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              type="button"
-              className="border border-solid border-white text-lg text-white py-1 px-3 xl:hover:border-rd xl:focus:border-rd transition-colors"
-              onClick={async () => {
-                const url = await createZip(imageset);
-
-                return handleDownload(
-                  `data:application/zip;base64,${url}`,
-                  `${user.name}-${activeImageset}.zip`
-                );
-              }}
-            >
-              DOWNLOAD: ALL
-            </motion.button>
-          )}
-        </AnimatePresence>
+        {user.fileCounts[activeImageset] ===
+        imageset.filter((item: object) => item instanceof File === true)
+          .length ? (
+          // encourages user to look through all their files first before downloading
+          // hugely resource intensive operation if immediately available
+          <button
+            type="button"
+            className="border border-solid border-white text-lg text-white py-1 px-3 xl:hover:border-rd xl:focus:border-rd transition-colors"
+            onClick={async () => {
+              const filtered = imageset.filter(
+                (item: object) => item instanceof File === true
+              );
+              await createZip(filtered, `${user.name}-${activeImageset}.zip`);
+            }}
+          >
+            DOWNLOAD: ALL
+          </button>
+        ) : (
+          <button
+            type="button"
+            disabled={disabled}
+            className="text-white border border-solid border-rd py-1 px-3 bg-black"
+            onClick={handleClick}
+          >
+            LOAD MORE
+          </button>
+        )}
       </div>
 
       <Carousel
--- client/src/components/user/dashboard/utils/createZip.ts
+++ client/src/components/user/dashboard/utils/createZip.ts
@@ -1,11 +1,14 @@
 import JSZip from "jszip";
+import { saveAs } from "file-saver";
 
-export const createZip = async (fileset: File[]) => {
+export const createZip = async (fileset: File[], zipName: string) => {
   const zip = new JSZip();
 
   for (let i = 0; i < fileset.length; i++) {
     zip.file(fileset[i].name, fileset[i]);
   }
 
-  return await zip.generateAsync({ type: "base64" });
+  return await zip
+    .generateAsync({ type: "blob" })
+    .then((blob) => saveAs(blob, zipName));
 };
