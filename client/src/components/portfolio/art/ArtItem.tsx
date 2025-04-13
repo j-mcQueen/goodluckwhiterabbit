112d0ec3cc87d6070d323f8fae83af3f64ea6038
jack mcqueen <jdmcqueen44@gmail.com>
2025-04-13 13:12:10 -0400

    Execute sequential animation on scroll

---
+++ client/src/components/portfolio/art/ArtItem.tsx
@@ -0,0 +1,90 @@
+import { motion, useAnimation, useInView } from "framer-motion";
+import { useEffect, useRef } from "react";
+
+export default function ArtItem({ ...props }) {
+  const { index, item } = props;
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: false });
+
+  const imgControls = useAnimation();
+  const listControls = useAnimation();
+
+  useEffect(() => {
+    const sequence = async () => {
+      await imgControls.start({
+        y: 0,
+        opacity: 1,
+        transition: { duration: 1.15 },
+      });
+
+      await listControls.start({
+        opacity: 1,
+        transition: {
+          duration: 1,
+          staggerChildren: 0.5,
+        },
+      });
+    };
+
+    if (isInView) sequence();
+  }, [isInView, imgControls, listControls]);
+
+  return (
+    <div
+      key={item.title}
+      ref={ref}
+      className="flex flex-col justify-center items-center text-white snap-start xl:h-[calc(100dvh-1.5rem-5dvw)] h-[calc(100dvh-1.5rem-25dvw)]"
+    >
+      <motion.img
+        initial={{ y: 25, opacity: 0 }}
+        // variants={img_variants}
+        // initial="initial"
+        // whileInView={"animate"}
+        animate={imgControls}
+        loading="lazy"
+        src={item.src}
+        alt={item.title}
+        className={`${index > 7 ? "xl:max-h-[35dvh]" : "xl:max-h-[60dvh]"} xl:max-w-[45dvw] max-w-[90dvw] max-h-[50dvh]`}
+      />
+
+      <motion.ul
+        // variants={list_variants}
+        // initial="initial"
+        // whileInView="animate"
+        initial={{ opacity: 0 }}
+        animate={listControls}
+        className="flex flex-col justify-center gap-1 pt-5 xl:pt-3 text-center tetx-md xl:text-lg"
+      >
+        <li>
+          <span className="italic">{item.title},&nbsp;</span>
+          {item.date}
+        </li>
+
+        <li>{item.medium}</li>
+
+        <li>{item.dims}</li>
+
+        <li>{item.signed ? "Signed" : "Unsigned"}</li>
+
+        <li>{item.edition === 1 ? "Unique" : `Edition of ${item.edition}`}</li>
+
+        <li
+          className={`${item.available ? "pt-4" : ""} font-vt tracking-wider`}
+        >
+          {item.available ? (
+            <a
+              href="mailto:goodluckwhiterabbit@gmail.com"
+              className="border border-solid border-white px-2 py-1 xl:hover:text-rd xl:focus:text-rd group xl:transition-colors xl:hover:drop-shadow-red xl:focus:drop-shadow-red xl:hover:border-rd xl:focus:border-rd drop-shadow-glo opacity-80"
+            >
+              INQUIRE
+            </a>
+          ) : (
+            <div className="drop-shadow-red text-rd pt-4">
+              <p>~ COLLECTED ~</p>
+            </div>
+          )}
+        </li>
+      </motion.ul>
+    </div>
+  );
+}
--- client/src/components/portfolio/art/Content.tsx
+++ client/src/components/portfolio/art/Content.tsx
@@ -1,29 +1,8 @@
 import { art_data } from "../../../assets/media/images/art/art_data";
 import { motion } from "framer-motion";
+import ArtItem from "./ArtItem";
 
 export default function Content() {
-  const img_variants = {
-    initial: {
-      y: 25,
-      opacity: 0,
-    },
-    animate: {
-      y: 0,
-      opacity: 1,
-      transition: {
-        duration: 1.15,
-      },
-    },
-  };
-
-  const list_variants = {
-    initial: { opacity: 0 },
-    animate: {
-      opacity: 1,
-      transition: { duration: 1, staggerChildren: 0.5 },
-    },
-  };
-
   const statement_variants = {
     initial: { opacity: 0 },
     animate: (j: number) => ({
@@ -67,59 +46,7 @@ export default function Content() {
               })}
             </div>
           ) : (
-            <div
-              key={item.title}
-              className="flex flex-col justify-center items-center text-white snap-start xl:h-[calc(100dvh-1.5rem-5dvw)] h-[calc(100dvh-1.5rem-25dvw)]"
-            >
-              <motion.img
-                variants={img_variants}
-                initial="initial"
-                whileInView={"animate"}
-                loading="lazy"
-                src={item.src}
-                alt={item.title}
-                className={`${index > 7 ? "xl:max-h-[35dvh]" : "xl:max-h-[60dvh]"} xl:max-w-[45dvw] max-w-[90dvw] max-h-[50dvh]`}
-              />
-
-              <motion.ul
-                variants={list_variants}
-                initial="initial"
-                whileInView="animate"
-                className="flex flex-col justify-center gap-1 pt-5 xl:pt-3 text-center tetx-md xl:text-lg"
-              >
-                <li>
-                  <span className="italic">{item.title},&nbsp;</span>
-                  {item.date}
-                </li>
-
-                <li>{item.medium}</li>
-
-                <li>{item.dims}</li>
-
-                <li>{item.signed ? "Signed" : "Unsigned"}</li>
-
-                <li>
-                  {item.edition === 1 ? "Unique" : `Edition of ${item.edition}`}
-                </li>
-
-                <li
-                  className={`${item.available ? "pt-4" : ""} font-vt tracking-wider`}
-                >
-                  {item.available ? (
-                    <a
-                      href="mailto:goodluckwhiterabbit@gmail.com"
-                      className="border border-solid border-white px-2 py-1 xl:hover:text-rd xl:focus:text-rd group xl:transition-colors xl:hover:drop-shadow-red xl:focus:drop-shadow-red xl:hover:border-rd xl:focus:border-rd drop-shadow-glo opacity-80"
-                    >
-                      INQUIRE
-                    </a>
-                  ) : (
-                    <div className="drop-shadow-red text-rd pt-4">
-                      <p>~ COLLECTED ~</p>
-                    </div>
-                  )}
-                </li>
-              </motion.ul>
-            </div>
+            <ArtItem key={item.title} index={index} item={item} />
           );
         })}
       </div>
