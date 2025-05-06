f2abac0501e1a926c10778f8079a70f9d18085c9
jack mcqueen <jdmcqueen44@gmail.com>
2025-05-05 21:46:13 -0400

    Extract first imageset load fn in prep for implementation

---
+++ client/src/components/admin/dashboard/types/handleFirstLoadTypes.ts
@@ -0,0 +1,36 @@
+import { Dispatch, SetStateAction } from "react";
+
+export interface handleFirstLoadTypes {
+  clients: { _id: string }[];
+  host: string;
+  newTargetImageset: string;
+  orderedImagesets: {
+    previews: (Blob | object)[];
+    full: (Blob | object)[];
+    socials: (Blob | object)[];
+    snips: (Blob | object)[];
+  };
+  setClients: Dispatch<SetStateAction<{ _id: string }[]>>;
+  setNotice: Dispatch<
+    SetStateAction<{
+      status: boolean;
+      message: string;
+      logout: { status: boolean; path: null | string };
+    }>
+  >;
+  setOrderedImagesets: Dispatch<
+    SetStateAction<{
+      previews: (Blob | object)[];
+      full: (Blob | object)[];
+      socials: (Blob | object)[];
+      snips: (Blob | object)[];
+    }>
+  >;
+  setSpinner: Dispatch<SetStateAction<boolean>>;
+  setStarted: Dispatch<SetStateAction<boolean>>;
+  setTargetClient: Dispatch<SetStateAction<{ _id: string }>>;
+  setTargetImageset: Dispatch<SetStateAction<string>>;
+  targetClient: {
+    _id: string;
+  };
+}
---
+++ client/src/components/admin/dashboard/utils/handlers/ordering/handleFirstLoad.tsx
@@ -0,0 +1,90 @@
+import { Dispatch, SetStateAction } from "react";
+import { updateOrderState } from "./updateOrderState";
+import { executeGenerationChain } from "../../../../../global/utils/executeGenerationChain";
+
+export const handleFirstLoad = async ({ ...params }) => {
+  const {
+    clients,
+    host,
+    newTargetImageset,
+    orderedImagesets,
+    setClients,
+    setNotice,
+    setOrderedImagesets,
+    setSpinner,
+    setStarted,
+    setTargetClient,
+    setTargetImageset,
+    targetClient,
+  } = params;
+
+  setTargetImageset(newTargetImageset);
+  setStarted(true);
+  setSpinner(true);
+
+  // TODO refactor executeGenerationChain
+
+  const data = await executeGenerationChain(
+    orderedImagesets[newTargetImageset as keyof typeof orderedImagesets],
+    newTargetImageset,
+    setNotice,
+    0,
+    10,
+    targetClient._id
+  );
+
+  try {
+    // TODO do we really need this? can't we just count what's in data?
+    const response = await fetch(
+      `${host}/admin/users/${targetClient._id}/${newTargetImageset}/getCount`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      }
+    );
+
+    const newCounts = await response.json();
+
+    if (newCounts && (response.status === 200 || response.status === 304)) {
+      // trigger state updates
+      const args: {
+        clients: { _id: string }[];
+        targetClient: { _id: string; fileCounts: number };
+        targetImageset: string;
+        setClients: Dispatch<SetStateAction<{ _id: string }[]>>;
+        setTargetClient: Dispatch<
+          SetStateAction<{ _id: string; fileCounts: number }>
+        >;
+        val: number;
+      } = {
+        clients,
+        targetClient,
+        targetImageset: newTargetImageset,
+        setClients,
+        setTargetClient,
+        val: newCounts,
+      };
+
+      await updateOrderState(args);
+    }
+  } catch (error) {
+    setNotice({
+      status: true,
+      message:
+        "There was an issue updating the number of files your client has in storage. Upon your next addition, the system will correct itself.",
+      logout: { status: false, path: null },
+    });
+  }
+
+  const nextOrderedImagesets = {
+    ...orderedImagesets,
+    [newTargetImageset]: data.files,
+  };
+  setOrderedImagesets(nextOrderedImagesets);
+  setSpinner(false);
+  return;
+};
