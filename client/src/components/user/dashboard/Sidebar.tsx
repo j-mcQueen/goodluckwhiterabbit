f877bca32a80b2813d17d6c2e9f398dab5d7f3c7
jack mcqueen <jdmcqueen44@gmail.com>
2025-06-29 02:10:29 -0400

    Extract sidebar

---
+++ client/src/components/user/dashboard/Sidebar.tsx
@@ -0,0 +1,86 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { handleDownloadAll } from "./utils/handleDownloadAll";
+import Loading from "../../global/Loading";
+
+export default function Sidebar({ ...props }) {
+  const {
+    activeImageset,
+    images,
+    retrieving,
+    setNotice,
+    setRetrieving,
+    spinner,
+    user,
+  } = props;
+
+  return (
+    <aside className="flex flex-col xl:gap-5 xl:min-w-[245px] xl:max-w-[245px] xl:max-h-[calc(100dvh-57px-1.5rem)] xl:border-r border-solid border-white">
+      <h1 className="text-white text-center text-2xl pt-5">
+        {user.name.toUpperCase()}
+      </h1>
+
+      <div className="text-center flex xl:block items-center justify-between px-5 xl:px-0 py-5 xl:py-0 border-b xl:border-none border-solid border-white">
+        <p className="text-white text-center xl:pb-2">
+          LOADED:{" "}
+          {
+            images[activeImageset as keyof typeof images].filter(
+              (img: Blob) => img instanceof Blob === true
+            ).length
+          }{" "}
+          / {user.fileCounts[activeImageset]}
+        </p>
+
+        <button
+          type="button"
+          className="border border-solid border-white text-lg text-white py-1 px-3 xl:hover:border-rd xl:hover:text-rd xl:hover:drop-shadow-red xl:focus:drop-shadow-red xl:focus:text-rd xl:focus:border-rd transition-colors xl:mt-5"
+          onClick={() => {
+            const args = {
+              activeImageset,
+              setNotice,
+              setRetrieving,
+              user,
+            };
+
+            handleDownloadAll(args);
+          }}
+        >
+          DOWNLOAD: ALL
+        </button>
+      </div>
+
+      <div>
+        {retrieving.state === true ? (
+          <AnimatePresence mode="wait">
+            {retrieving.state && (
+              <motion.p
+                key={"notice"}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                className="text-ylw min-w-[245px] max-w-[245px] px-5 pt-5"
+              >
+                Your download has started. Please keep this page open while it
+                completes!
+              </motion.p>
+            )}
+          </AnimatePresence>
+        ) : retrieving.complete ? (
+          <motion.p
+            key={"notice"}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            className="text-blu min-w-[245px] max-w-[245px] px-5 pt-5"
+          >
+            Download complete. Enjoy!
+          </motion.p>
+        ) : null}
+
+        {spinner ? (
+          <div className="pt-5 text-center">
+            <Loading />
+          </div>
+        ) : null}
+      </div>
+    </aside>
+  );
+}
--- client/src/components/user/dashboard/Views.tsx
+++ client/src/components/user/dashboard/Views.tsx
@@ -1,11 +1,9 @@
 import { useState } from "react";
-import { handleDownloadAll } from "./utils/handleDownloadAll";
-import { AnimatePresence, motion } from "framer-motion";
 import { mobile } from "./utils/determineViewport";
 
 import Carousel from "./views/Carousel";
-import Loading from "../../global/Loading";
 import Scroller from "./views/Scroller";
+import Sidebar from "./Sidebar";
 
 export default function Views({ ...props }) {
   const {
@@ -25,74 +23,15 @@ export default function Views({ ...props }) {
 
   return (
     <div className="flex xl:flex-row flex-col max-h-[calc(100dvh-57px-1.5rem)]">
-      <aside className="flex flex-col xl:gap-5 xl:min-w-[245px] xl:max-w-[245px] xl:max-h-[calc(100dvh-57px-1.5rem)] xl:border-r border-solid border-white">
-        <h1 className="text-white text-center text-2xl pt-5">
-          {user.name.toUpperCase()}
-        </h1>
-
-        <div className="text-center flex xl:block items-center justify-between px-5 xl:px-0 py-5 xl:py-0 border-b xl:border-none border-solid border-white">
-          <p className="text-white text-center xl:pb-2">
-            LOADED:{" "}
-            {
-              images[activeImageset as keyof typeof images].filter(
-                (img: Blob) => img instanceof Blob === true
-              ).length
-            }{" "}
-            / {user.fileCounts[activeImageset]}
-          </p>
-
-          <button
-            type="button"
-            className="border border-solid border-white text-lg text-white py-1 px-3 xl:hover:border-rd xl:hover:text-rd xl:hover:drop-shadow-red xl:focus:drop-shadow-red xl:focus:text-rd xl:focus:border-rd transition-colors xl:mt-5"
-            onClick={() => {
-              const args = {
-                activeImageset,
-                user,
-                setNotice,
-                setRetrieving,
-              };
-
-              handleDownloadAll(args);
-            }}
-          >
-            DOWNLOAD: ALL
-          </button>
-        </div>
-
-        <div>
-          {retrieving.state === true ? (
-            <AnimatePresence mode="wait">
-              {retrieving.state && (
-                <motion.p
-                  key={"notice"}
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  exit={{ opacity: 0 }}
-                  className="text-ylw min-w-[245px] max-w-[245px] px-5 pt-5"
-                >
-                  Your download has started. Please keep this page open while it
-                  completes!
-                </motion.p>
-              )}
-            </AnimatePresence>
-          ) : retrieving.complete ? (
-            <motion.p
-              key={"notice"}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              className="text-blu min-w-[245px] max-w-[245px] px-5 pt-5"
-            >
-              Download complete. Enjoy!
-            </motion.p>
-          ) : null}
-
-          {spinner ? (
-            <div className="pt-5 text-center">
-              <Loading />
-            </div>
-          ) : null}
-        </div>
-      </aside>
+      <Sidebar
+        activeImageset={activeImageset}
+        images={images}
+        retrieving={retrieving}
+        setNotice={setNotice}
+        setRetrieving={setRetrieving}
+        spinner={spinner}
+        user={user}
+      />
 
       {!mobile ? (
         <Carousel
