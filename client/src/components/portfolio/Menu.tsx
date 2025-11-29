4b3ab6ec5504520e977d1b1d44577e4a615248f8
jack mcqueen <jdmcqueen44@gmail.com>
2025-11-29 17:37:03 -0500

    Data extraction/restructuring for cleanliness

--- client/src/components/portfolio/Menu.tsx
+++ client/src/components/portfolio/Menu.tsx
@@ -20,58 +20,19 @@ export default function Menu({ ...props }) {
     }),
   };
 
+  const subcategories = [
+    "WEDDINGS",
+    "EVENTS",
+    "FILM",
+    "COMMERCIAL",
+    "EDITORIAL",
+  ];
+
   // 4 scenarios need accounted for:
   // 1. User loads into page and views first set of images on photography + weddings page
   // 2. User scrolls to load next set of images (handleIntersection)
   // 3. User clicks on a sidebar item to load a different category
   // 4. What happens to the UI when a user loads enough images to scroll to the next sub-category
-
-  const groups = [
-    {
-      "MAUREEN + JOE": "001",
-      "MARILEE + JAMES": "002",
-      "9 ORCHARD": "003",
-      MONROE: "004",
-      "": "005",
-      "THE RIVER CAFE": "006",
-      "M&R I": "007",
-      "FANDI MATA": "008",
-      "LE FANFARE": "009",
-      "CARROLL GARDEN": "010",
-      TUFFET: "011",
-      "TIME 100": "012",
-      "THE BOX HOUSE": "013",
-      "M&R II": "014",
-      "BK WINERY": "015",
-      "501 UNION": "016",
-      "THE SANCTUARY": "017",
-      "RY x MW x TAG": "018",
-    },
-    {
-      "EVENT 1": "001",
-      "EVENT 2": "002",
-      "EVENT 3": "003",
-      "EVENT 4": "004",
-    },
-    {
-      "FILM 1": "001",
-      "FILM 2": "002",
-      "FILM 3": "003",
-      "FILM 4": "004",
-    },
-    {
-      "COMMERCIAL 1": "001",
-      "COMMERCIAL 2": "002",
-      "COMMERCIAL 3": "003",
-      "COMMERCIAL 4": "004",
-    },
-    {
-      "EDITORIAL 1": "001",
-      "EDITORIAL 2": "002",
-      "EDITORIAL 3": "003",
-      "EDITORIAL 4": "004",
-    },
-  ];
 
   return (
     <ul className="flex flex-row-reverse h-full [writing-mode:sideways-lr]">
@@ -98,7 +59,7 @@ export default function Menu({ ...props }) {
                 <ul
                   className={`${activeSub !== index ? "invisible" : ""} overflow-x-scroll max-w-[189px] [writing-mode:vertical-rl]`}
                 >
-                  {Object.keys(groups[index]).map(
+                  {Object.keys(data[index]).map(
                     (project: string, j: number) => {
                       return (
                         <li
@@ -127,7 +88,7 @@ export default function Menu({ ...props }) {
                     }}
                     className={`border border-white border-solid p-3 py-5 w-[56px] h-full ${index === data.length - 1 ? "border-b-0" : null} ${index === activeSub ? "border-r-black text-ylw " : null} xl:hover:text-ylw xl:hover:transition-colors [writing-mode:vertical-rl]`}
                   >
-                    {category.title}
+                    {subcategories[index]}
                   </button>
                 </div>
               </motion.li>
---
+++ client/src/components/portfolio/data/sidebar/data.ts
@@ -0,0 +1,62 @@
+export const sidebar_data = {
+  "/art": {
+    title: "ART",
+    bio: "LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE MAGNA ALIQUA.",
+    menu: [{ title: "CLOUDSCAPES" }],
+  },
+  "/design": {
+    title: "DESIGN",
+    bio: "LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE MAGNA ALIQUA.",
+    menu: [],
+  },
+  "/photo": {
+    title: "PHOTOGRAPHY",
+    bio: "LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE MAGNA ALIQUA.",
+    menu: [
+      {
+        "MAUREEN + JOE": "001",
+        "MARILEE + JAMES": "002",
+        "9 ORCHARD": "003",
+        MONROE: "004",
+        "": "005",
+        "THE RIVER CAFE": "006",
+        "M&R I": "007",
+        "FANDI MATA": "008",
+        "LE FANFARE": "009",
+        "CARROLL GARDEN": "010",
+        TUFFET: "011",
+        "TIME 100": "012",
+        "THE BOX HOUSE": "013",
+        "M&R II": "014",
+        "BK WINERY": "015",
+        "501 UNION": "016",
+        "THE SANCTUARY": "017",
+        "RY x MW x TAG": "018",
+      },
+      {
+        "EVENT 1": "001",
+        "EVENT 2": "002",
+        "EVENT 3": "003",
+        "EVENT 4": "004",
+      },
+      {
+        "FILM 1": "001",
+        "FILM 2": "002",
+        "FILM 3": "003",
+        "FILM 4": "004",
+      },
+      {
+        "COMMERCIAL 1": "001",
+        "COMMERCIAL 2": "002",
+        "COMMERCIAL 3": "003",
+        "COMMERCIAL 4": "004",
+      },
+      {
+        "EDITORIAL 1": "001",
+        "EDITORIAL 2": "002",
+        "EDITORIAL 3": "003",
+        "EDITORIAL 4": "004",
+      },
+    ],
+  },
+};
