d5af85fb062678d310766aeaf8421b419a58363e
jack mcqueen <jdmcqueen44@gmail.com>
2026-06-27 16:49:31 -0400

    Fix: append new files to queue on input change

--- client/src/components/admin/dashboard/utils/handlers/queueing/handleChange.ts
+++ client/src/components/admin/dashboard/utils/handlers/queueing/handleChange.ts
@@ -5,9 +5,9 @@ import { resize } from "../../compress/resize";
 export const handleChange = async (
   e: React.ChangeEvent<HTMLInputElement>,
   setUploadCount: Dispatch<SetStateAction<number>>,
-  setQueue: Dispatch<SetStateAction<File[]>>
+  setQueue: Dispatch<SetStateAction<File[]>>,
 ) => {
-  const placeholder = [];
+  const placeholder: File[] = [];
   if (e.target.files) {
     // resize all files
     for (let i = 0; i < e.target.files.length; i++) {
@@ -16,19 +16,19 @@ export const handleChange = async (
         e.target.files[i].type,
         0.1,
         0.5,
-        0.9
+        0.9,
       );
       const filename = `c_${e.target.files[i].name}`;
       const converted = convertToFile(
         resized,
         filename,
-        e.target.files[i].type
+        e.target.files[i].type,
       );
       const newCount = placeholder.length + 1;
       setUploadCount(newCount);
       placeholder.push(converted);
     }
-    setQueue([...placeholder]);
+    setQueue((prev) => [...prev, ...placeholder]);
   }
   return;
 };
